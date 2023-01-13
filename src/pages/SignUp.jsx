5f61b6ee3f32b8f353c98dd456dc50dcac579bab
JeongEun Heo <118269129+wjddms0501@users.noreply.github.com>
2023-01-14 03:15:51 +0900

    Design - 인증 시 disabled 처리

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,3 @@
-import Agree from "../components/SignUp/Agree";
 import Registration from "../components/SignUp/Registration";
 import Agree2 from "../components/SignUp/Agree2";
 
@@ -6,7 +5,6 @@ const SignUp = () => {
   return (
     <div>
       <Agree2 />
-      {/* <Agree /> */}
       <Registration />
     </div>
   );
