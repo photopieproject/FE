e1c5281c1825b3712ca1e7adf81a6009cb0ace53
chan <chan@chanui-MacBookAir.local>
2023-01-30 20:55:39 +0900

    Fix - fixed pub and sub camera

--- src/components/OvVideo/OvVideo.js
+++ src/components/OvVideo/OvVideo.js
@@ -14,28 +14,3 @@ const OpenViduVideoComponent = (props) => {
 };
 
 export default OpenViduVideoComponent;
-
-/*
-export default class OpenViduVideoComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.videoRef = React.createRef();
-    }
-
-    // componentDidUpdate(props) {
-    //     if (props && !!this.videoRef) {
-    //         this.props.streamManager.addVideoElement(this.videoRef.current);
-    //     }
-    // }
-
-    componentDidMount() {
-        if (this.props && !!this.videoRef) {
-            this.props.streamManager.addVideoElement(this.videoRef.current);
-        }
-    }
-
-    render() {
-        return <video autoPlay={true} ref={this.videoRef} />;
-    }
-}
-*/
