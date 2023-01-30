adf78dc173f3155cbbc72b7d4d8b79dd8ba02beb
JeongEun Heo <118269129+wjddms0501@users.noreply.github.com>
2023-01-30 20:47:32 +0900

    Docs - 카카오톡 공유하기 수정

--- src/components/Kakao/ShareKakao.js
+++ src/components/Kakao/ShareKakao.js
@@ -1,60 +1,130 @@
 import { useEffect } from "react";
 
 export const ShareKakao = (photoUrl) => {
-  const ShareKakaoSend = (photoUrl) => {
-    // * photoUrl = roomId에서 촬영한 사진 4장 + 프레임 합친 이미지 url (카톡 공유할 사진)
-    // * REACT_APP_SHARE_KAKAO_LINK_KEY = 8b4bfa212ce98720d4df490e0758420e // env 파일에 추가, 외부로 공개되면 안됨
+  // 왜 안되는지 모르겠는 애
+  //   useEffect(() => {
+  //     ShareKakaoSend();
+  //   }, []);
 
-    //16~18줄 대신 써봄 -> "Kakao init:App key must be provided" 에러문구 사라짐
-    //근데 process.env.REACT_APP_SHARE_KAKAO_LINK_KEY의 변수 지정 후
-    // console.log(변수) 했을 때 콘솔이 안찍힘 ㅠㅠ
-    useEffect(() => {
-      window.Kakao.init(process.env.REACT_APP_SHARE_KAKAO_LINK_KEY);
-    }, []);
+  //   const ShareKakaoSend = () => {
+  //     if (window.Kakao) {
+  //       const kakao = window.Kakao;
 
-    // roomId에 따라 다른 url을 보내줄거기 때문에 photoUrl를 인자값으로 받아줌
+  //       if (!kakao.isInitialized()) {
+  //         kakao.init("8b4bfa212ce98720d4df490e0758420e"); // 카카오에서 제공 받은 javascript key를 넣어줌 -> .env파일에서 호출시킴
+  //         // kakao.init(process.env.REACT_APP_SHARE_KAKAO_LINK_KEY); // 카카오에서 제공 받은 javascript key를 넣어줌 -> .env파일에서 호출시킴
+  //       }
+  //       // kakao.Link.sendDefault({
+  //       kakao.Link.sendDefaultButton({
+  //         container: "#kakao-link-btn",
+  //         objectType: "feed", // 카카오 링크 공유 여러 type들 중 feed라는 타입
+  //         content: {
+  //           title: "Photopie", // 제목
+  //           description: "함께 찍은 사진을 친구와 공유하고 다운로드 해보세요!", // 설명
+  //           imageUrl:
+  //             "https://my-photopie-github-actions-s3-bucket.s3.ap-northeast-2.amazonaws.com/frame/rainbow.jpg", // 일단 임시 url로 넣어 놨고, 추후 화질 좋고 예쁜(?) 포토파이 로고로 수정
+  //           link: {
+  //             mobileWebUrl: "https://photo-pie.shop",
+  //             // photoUrl, // imageUrl 클릭시 다운로드 할 이미지 링크로 이동하는 것 같음
+  //             //"https://photo-pie.shop/api/photo/kakaodata"
+
+  //             webUrl: "https://photo-pie.shop",
+  //             // photoUrl,
+  //           },
+  //         },
+  //         buttons: [
+  //           {
+  //             title: "다운로드", // 버튼 제목
+  //             link: {
+  //               mobileWebUrl: "https://photo-pie.shop",
+  //               // photoUrl, // ‘다운로드‘ 버튼 클릭시 다운로드 할 이미지 링크로 이동
+  //               webUrl: "https://photo-pie.shop",
+  //               // photoUrl,
+  //             },
+  //           },
+  //         ],
+  //       });
+  //     }
+  //   };
+
+  //   return (
+  //     <div>
+  //       <button
+  //         id="kakao-link-btn"
+  //         //  onClick={() => ShareKakaoSend(photoUrl)}
+  //       >
+  //         {/* <img
+  //           src={`${process.env.PUBLIC_URL}/assets/KakaoLogo.png`}
+  //           alt={"Kakao Logo"}
+  //         /> */}
+  //       </button>
+  //     </div>
+  //   );
+  // };
+
+  //--------------새로 아래
+  useEffect(() => {
+    ShareKakaoSend();
+  }, []);
+
+  const ShareKakaoSend = () => {
+    // kakao sdk script이 정상적으로 불러와졌으면 window.Kakao로 접근이 가능합니다
     if (window.Kakao) {
       const kakao = window.Kakao;
-      // if (!kakao.isInitialized()) {
-      //   kakao.init(process.env.REACT_APP_SHARE_KAKAO_LINK_KEY); // 카카오에서 제공 받은 javascript key를 넣어줌 -> .env파일에서 호출시킴
-      // }
 
-      kakao.Link.sendDefault({
-        objectType: "feed", // 카카오 링크 공유 여러 type들 중 feed라는 타입
+      // 중복 initialization 방지
+      if (!kakao.isInitialized()) {
+        // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
+        kakao.init("8b4bfa212ce98720d4df490e0758420e");
+        // kakao.init("process.env.REACT_APP_SHARE_KAKAO_LINK_KEY");
+        //위에는 인증실패로 뜸, .env에서 못가져오나봄
+      }
+
+      kakao.Link.createDefaultButton({
+        // Render 부분 id=kakao-link-btn 을 찾아 그부분에 렌더링을 합니다
+        container: "#kakao-link-btn",
+        objectType: "feed",
         content: {
-          title: "Photopie", // 제목
-          description: "함께 찍은 사진을 친구와 공유하고 다운로드 해보세요!", // 설명
+          title: "Photo-Pie",
+          description: "함께 찍은 사진을 공유하고 다운로드 해보세요!",
           imageUrl:
-            "https://my-photopie-github-actions-s3-bucket.s3.ap-northeast-2.amazonaws.com/frame/rainbow.jpg", // 일단 임시 url로 넣어 놨고, 추후 화질 좋고 예쁜(?) 포토파이 로고로 수정
+            "https://my-photopie-github-actions-s3-bucket.s3.ap-northeast-2.amazonaws.com/frame/rainbow.jpg", // i.e. process.env.FETCH_URL + '/logo.png'
           link: {
-            mobileWebUrl: photoUrl, // imageUrl 클릭시 다운로드 할 이미지 링크로 이동하는 것 같음
-            //"https://photo-pie.shop/api/photo/kakaodata"
-
-            webUrl: photoUrl,
+            mobileWebUrl: "https://dev.djcf93g3uh9mz.amplifyapp.com",
+            webUrl: "https://dev.djcf93g3uh9mz.amplifyapp.com",
           },
         },
         buttons: [
           {
-            title: "다운로드", // 버튼 제목
+            title: "다운로드",
             link: {
-              mobileWebUrl: photoUrl, // ‘다운로드‘ 버튼 클릭시 다운로드 할 이미지 링크로 이동
-              webUrl: photoUrl,
+              mobileWebUrl: "https://dev.djcf93g3uh9mz.amplifyapp.com",
+              webUrl: "https://dev.djcf93g3uh9mz.amplifyapp.com",
             },
           },
+          // {
+          //   title: "앱으로 보기",
+          //   link: {
+          //     mobileWebUrl: "https://dev.djcf93g3uh9mz.amplifyapp.com",
+          //     webUrl: "https://dev.djcf93g3uh9mz.amplifyapp.com",
+          //   },
+          // },
         ],
       });
     }
   };
 
   return (
-    <div>
-      <button onClick={() => ShareKakaoSend(photoUrl)}>
-        <img
-          src={`${process.env.PUBLIC_URL}/assets/KakaoLogo.png`}
-          alt={"Kakao Logo"}
-        />
+    <div className="kakao-share-button">
+      <button
+        id="kakao-link-btn"
+        // onClick={ShareKakaoSend(photoUrl)}
+      >
+        <img src="/icons/kakao.png" alt="kakao-share-icon" />
       </button>
     </div>
   );
 };
+//--------------새로 위
+
 export default ShareKakao;
--- src/pages/PhotoSave.jsx
+++ src/pages/PhotoSave.jsx
@@ -18,7 +18,9 @@ const PhotoSave = () => {
     script.src = "https://developers.kakao.com/sdk/js/kakao.js";
     script.async = true;
     document.body.appendChild(script);
-    return () => document.body.removeChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
   }, []);
 
   $(function () {
